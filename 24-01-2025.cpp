d5fae556b2450f811da1022cc3ec9430fb793964
Chhotelal Yadav <168395356+HiChhotelal@users.noreply.github.com>
2025-01-24 20:27:07 +0530

    Add files via upload

---
+++ 24-01-2025.cpp
@@ -0,0 +1,30 @@
+class Solution {
+  public:
+   
+    bool detectLoop(Node* head) {
+        // your code here
+        
+        if(!head || !head->next){
+            return false;
+        }
+
+        Node *slow = head;
+        Node *fast = head;
+
+        while(true){
+
+            if(fast->next && fast->next->next){
+                slow = slow->next;
+                fast = fast->next->next;
+
+                if(slow == fast){
+                    return true;
+                }
+            }else{
+                break;
+            }
+        }
+
+        return false;
+    }
+};
