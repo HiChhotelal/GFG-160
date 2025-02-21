712dd7485d0fd40ca79d22189330e98c1bf6db34
Chhotelal Yadav <168395356+HiChhotelal@users.noreply.github.com>
2025-02-21 21:11:58 +0530

    Create 22-02-2025.cpp

---
+++ 22-02-2025.cpp
@@ -0,0 +1,20 @@
+class Solution {
+  public:
+    bool isBalanced(string& s) {
+        // code here
+        stack<char> st;
+          for(int i=0;i<s.size();i++){
+              if(st.empty() || s[i] == '(' || s[i] == '{' || s[i] == '['){
+                  st.push(s[i]);
+              }else{
+                  if(s[i] == ')' && st.top() == '(') st.pop();
+                  else if(s[i] == '}' && st.top() == '{') st.pop();
+                  else if(s[i] == ']' && st.top() == '[') st.pop();
+                  else return false;
+              }
+          }
+          if(st.empty()) return true;
+          return false;
+        
+    }
+};
